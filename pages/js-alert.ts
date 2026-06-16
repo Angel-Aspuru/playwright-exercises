84b4de4bbf3f65b6acdddd5bb77c33d0437597fc
Angel Aspuru <angel.aspuru@unosquare.com>
2026-06-16 11:56:57 -0600

    last fixes for the js alert test

--- pages/js-alert.ts
+++ pages/js-alert.ts
@@ -1,12 +1,9 @@
-    import type { Page, Locator } from "@playwright/test";
+import type { Page, Locator } from "@playwright/test";
 import { expect } from "@playwright/test";
-
 
 export class JsAlertPage {
     private readonly page: Page;
     private readonly alertButton: Locator;
-
-
 
     constructor(page: Page) {
         this.page = page;
@@ -16,17 +13,23 @@ export class JsAlertPage {
 
     async goto() {
         await this.page.goto('https://the-internet.herokuapp.com/javascript_alerts');
-        await expect(this.page).toHaveURL(/javascripts_alerts/);
+        await expect(this.page).toHaveURL(/javascript_alerts/);
     }
 
     async clickAlertButton() {
-        this.alertButton.click();
-
-        this.page.on('dialog', async dialog => {
-            console.log(dialog.message);
-            await dialog.accept();
-            this.page.screenshot()
-        })
+        // tie the dialog handling into the test flow using Promise.all
+        // waitForEvent('dialog') is awaited
+        // Playwright waits until:
+        // dialog appears
+        // handler finishes
+        // screenshot completes before test ends
+        await Promise.all([
+            //to only have it listen once and not active forever, it can be before the click since this wait until the alert 
+            this.page.waitForEvent('dialog').then(async dialog => {
+                expect(dialog.message()).toBe('I am a JS Alert');
+                await dialog.accept();
+            }),
+            this.alertButton.click()
+        ]);
     }
-
 }
