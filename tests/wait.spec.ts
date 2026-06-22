a8cc47aa75d7f62c141df30b59dab92c193445e7
Angel Aspuru <angel.aspuru@unosquare.com>
2026-06-22 17:55:42 -0600

    removing innecesary import

--- tests/wait.spec.ts
+++ tests/wait.spec.ts
@@ -1,7 +1,7 @@
 import {test, expect }from "@playwright/test";
 import type { Page } from "@playwright/test";
 import { AdvanceWiatPage } from "../pages/advance-waits";
-import { waitForDebugger } from "node:inspector";
+
 
 test.describe.configure({mode: 'serial'});
 
