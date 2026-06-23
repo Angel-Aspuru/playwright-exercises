bc26ee527494c1710709835bcf4ecc7661dec4de
Angel Aspuru <angel.aspuru@unosquare.com>
2026-06-23 13:36:31 -0600

    forgot to add to the commit a edit I made in the frame page

--- pages/frames.ts
+++ pages/frames.ts
@@ -4,11 +4,13 @@ export class FramePage {
     private readonly page: Page;
     readonly tinyMCEAlert: Locator;
     readonly pageIframe: Locator;
+    readonly mainHeader: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.tinyMCEAlert = page.getByRole('alert').getByRole('button');
         this.pageIframe = page.frameLocator('#mce_0_ifr').getByLabel('Rich Text Area. Press ALT-0 for help.');
+        this.mainHeader = page.getByRole('heading', { level: 3 }).filter({ hasText: 'TinyMCE WYSIWYG Editor' }).nth(0);
     };
 
     async goto() {
